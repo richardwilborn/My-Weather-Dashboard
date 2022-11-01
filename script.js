69bb19e15abe8436b4ecb1f75d7edee7102631e6
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-01 11:36:47 -0500

    added timezone pllugins to day.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-
-
+//global variables
+var weatherApiRootUrl = 'https://api.openweathermap.org';
 var weatherApiKey = 'e80a21913ec0a12015733c7bd52e62b8';
+
+//timezones
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
