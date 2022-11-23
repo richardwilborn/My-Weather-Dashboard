76d81afe7eccc2e215c5f0b1a46bab4b9d090b39
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-23 10:38:07 -0600

    added vars from api and elements for card

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ var weatherApiKey = 'e80a21913ec0a12015733c7bd52e62b8';
 //timezones
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
+
+function renderForecastCard(forecast, timezone) {
+  //vars from api
+  var unixTs = forecast.dt;
+  var iconUrl = 'http://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png';
+  var iconDescription = forecast.weather[0].description;
+  var tempF = forecast.temp.day;
+  var { humidity } = forecast;
+  var windMph = forecast.wind_speed;
+
+  // elements creation
+  var col = document.createElement('div');
+  var card = document.createElement('div');
+  var cardBody = document.createElement('div');
+  var cardTitle = document.createElement('h5');
+  var weatherIcon = document.createElement('img');
+  var tempEl = document.createElement('p');
+  var windEl = document.createElement('p');
+  var humidityEl = document.createElement('p');
+}
